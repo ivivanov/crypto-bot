f2e73212c049c17975af5d1a1012121b85949a58
ivivanov <ivanivanov12@gmail.com>
2022-12-03 19:51:25 +0200

    Remove debug logs

--- app/prepare.go
+++ app/prepare.go
@@ -24,8 +24,6 @@ func (p *Preparer) OpenBuyOrders(bank, price, grid, orders float64) error {
 		currPrice = helper.Round5dec(currPrice - currPrice*grid/100)
 		clientOrderID := helper.GetClientOrderID(p.bot.account, currPrice)
 
-		log.Print(len(clientOrderID))
-		log.Print(clientOrderID)
 		resp, err := p.bot.limitOrdersCreator.PostBuyLimitOrder(p.bot.pair, clientOrderID, amount, currPrice)
 		if err != nil {
 			return err
