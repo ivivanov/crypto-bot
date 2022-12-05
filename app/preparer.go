9b15fffee06565553608552c731b5bcc76628b7c
ivivanov <ivanivanov12@gmail.com>
2022-12-05 22:08:03 +0200

    Add prepare cmd for both buy&sell

--- app/preparer.go
+++ app/preparer.go
@@ -57,3 +57,39 @@ func (p *Preparer) OpenBuyOrders(bank, price, grid, orders float64) error {
 
 	return nil
 }
+
+func (p *Preparer) OpenSellOrders(bank, price, grid, orders float64) error {
+	currPrice := price
+	amount := helper.Round2dec(bank / orders)
+
+	for i := 0; i < int(orders); i++ {
+		currPrice = helper.Round5dec(currPrice + currPrice*grid/100)
+		clientOrderID := helper.GetClientOrderID(p.account, currPrice)
+
+		resp, err := p.limitOrdersCreator.PostSellLimitOrder(p.pair, clientOrderID, amount, currPrice)
+		if err != nil {
+			return err
+		}
+
+		if resp.IsError() {
+			log.Print(resp.Reason)
+			return fmt.Errorf("failed to create order")
+		}
+
+		log.Printf("Order-created-> %v: %v @ %v", "sell", resp.Amount, resp.Price)
+	}
+
+	return nil
+}
+
+func (p *Preparer) OpenBuySellOrders(bank, price, grid, orders float64) error {
+	if err := p.OpenBuyOrders(bank/2, price, grid, orders/2); err != nil {
+		return err
+	}
+
+	if err := p.OpenSellOrders(bank/2, price, grid, orders/2); err != nil {
+		return err
+	}
+
+	return nil
+}
--- cmd/prepare.go
+++ cmd/prepare.go
@@ -9,11 +9,20 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type OrderType int8
+
+const (
+	Buy  uint8 = 0
+	Sell uint8 = 1
+	Both uint8 = 2
+)
+
 var (
-	price  float64
-	bank   float64
-	grid   float64
-	orders float64
+	price      float64
+	bank       float64
+	grid       float64
+	orders     float64
+	ordersType uint8
 )
 
 var prepareCmd = &cobra.Command{
@@ -24,7 +33,15 @@ For this reason we need to create the initial orders based on few configurations
 	Run: func(cmd *cobra.Command, args []string) {
 		preparer, err := app.NewPreparer(account, apiKey, apiSecret, customerID, pair, verbose)
 		helper.HandleFatalError(err)
-		helper.HandleFatalError(preparer.OpenBuyOrders(bank, price, grid, orders))
+
+		switch ordersType {
+		case Buy:
+			helper.HandleFatalError(preparer.OpenBuyOrders(bank, price, grid, orders))
+		case Sell:
+			helper.HandleFatalError(preparer.OpenSellOrders(bank, price, grid, orders))
+		case Both:
+			helper.HandleFatalError(preparer.OpenBuySellOrders(bank, price, grid, orders))
+		}
 	},
 }
 
@@ -35,6 +52,7 @@ func init() {
 	prepareCmd.Flags().Float64Var(&bank, "bank", 0, "Start capital")
 	prepareCmd.Flags().Float64Var(&grid, "grid", 0.04, "Grid step %")
 	prepareCmd.Flags().Float64Var(&orders, "orders", 6, "Initial orders count")
+	prepareCmd.Flags().Uint8Var(&ordersType, "type", 0, "0 - only buy; 1 - only sell; 2 - both")
 
 	prepareCmd.MarkFlagRequired("price")
 	prepareCmd.MarkFlagRequired("bank")
